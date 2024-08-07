cf47cb02db92738daee729601a56f5ff3ba42491
Raminoheritiana Nomena <raminoheritiana.nr@pg.com>
2024-08-07 12:07:41 +0300

    [dev][builder] fix cursor when image hover

---
+++ src/builder/src/Utils/HTML/getElementInfo.tsx
@@ -0,0 +1,49 @@
+export interface ElementInfo {
+    top: number,
+    right: number,
+    bottom: number,
+    left: number,
+    width: number,
+    height: number,
+    x: number,
+    y: number,
+    display: string
+}
+export default function getElementInfo(element: HTMLElement):ElementInfo|null {
+    // Vérifie si l'élément est valide
+    if (!element || !(element instanceof HTMLElement)) {
+        console.error('L\'élément fourni n\'est pas valide.');
+        return null;
+    }
+
+    // Utilise getBoundingClientRect pour obtenir les informations
+    const rect = element.getBoundingClientRect();
+    const style = window.getComputedStyle(element);
+
+    console.log(
+        {
+            top: rect.top,
+            right: rect.right,
+            bottom: rect.bottom,
+            left: rect.left,
+            width: rect.width,
+            height: rect.height,
+            x: rect.x,
+            y: rect.y,
+            display: style.display
+        }
+    )
+    // Retourne les informations pertinentes
+    return {
+        top: rect.top,
+        right: rect.right,
+        bottom: rect.bottom,
+        left: rect.left,
+        width: rect.width,
+        height: rect.height,
+        x: rect.x,
+        y: rect.y,
+        display: style.display
+    };
+}
+
--- src/builder/src/components/HoverElement/HoverImageBorder.tsx
+++ src/builder/src/components/HoverElement/HoverImageBorder.tsx
@@ -1,8 +1,23 @@
 'use client'
 
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import getElementInfo, {ElementInfo} from "@/builder/src/Utils/HTML/getElementInfo";
+import {Box} from "@mui/material";
+
+const defaultCursorElementInfoProps = {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0,
+    display: 'none'
+}
 
 const HoverImageBorder = () => {
+    const [cursorElementInfo, setCursorElementInfo] = useState<ElementInfo|null>(null);
     useEffect(() => {
         const interval = setInterval(() => {
             const imageElements = document.querySelectorAll('img');
@@ -10,23 +25,52 @@ const HoverImageBorder = () => {
                 if(!image.dataset['mouseover']) {
                     image.dataset['mouseover'] = String(true);
                     image.addEventListener('mouseover', (e) => {
-                        const parentNode = image ? image.parentElement : null;
-                        parentNode && parentNode.classList.add('img-container')
-                    })
-                }
+                       const imageInfo = getElementInfo(image);
+                       if(imageInfo) {
+                           setCursorElementInfo({
+                               ...imageInfo,
+                               top: imageInfo.top +  window.scrollY,
+                               left: imageInfo.left + window.scrollX,
+                               width: imageInfo.width - 1,
+                               height: imageInfo.height,
+                               display: 'block'
+                           })
+                       }
 
-                if(!image.dataset['mouseout']) {
-                    image.dataset['mouseout'] = String(true);
+                    })
                     image.addEventListener('mouseout', (e) => {
-                        const parentNode = image ? image.parentElement : null;
-                        parentNode && parentNode.classList.remove('img-container')
+                        setCursorElementInfo(defaultCursorElementInfoProps)
                     })
                 }
             })
         }, 1000);
-        setTimeout(() => clearInterval(interval), 60000)
+        setTimeout(() => clearInterval(interval), 60000);
+
+        return () => {
+            const imageElements = document.querySelectorAll('img');
+            imageElements.forEach((image:HTMLImageElement) => {
+                image.removeEventListener('mouseover',() => {})
+                image.removeEventListener('mouseout',() => {})
+            })
+        }
     })
-   return <></>
+   return <>
+       {
+           cursorElementInfo &&
+           <Box sx={{
+               display: cursorElementInfo.display,
+               top: cursorElementInfo.top,
+               left: cursorElementInfo.left,
+               position: 'absolute',
+               zIndex: 999999,
+               width: cursorElementInfo.width,
+               height: cursorElementInfo.height,
+               border: '2px solid aqua',
+               pointerEvents: 'none'
+           }}>
+           </Box>
+       }
+   </>
 }
 
 export default HoverImageBorder
