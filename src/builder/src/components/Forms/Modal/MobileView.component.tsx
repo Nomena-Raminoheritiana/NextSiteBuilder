6702e87d51f867c5838e22fca2ddb303339cdb61
Nomena Raminoheritiana <n.raminoheritiana@gmail.com>
2024-08-29 15:46:18 +0300

    [dev][builder] fix import issue

--- src/builder/src/components/Forms/Modal/MobileView.component.tsx
+++ src/builder/src/components/Forms/Modal/MobileView.component.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import MainModal from "@/builder/src/components/Forms/Modal/MainModal";
-import {Box, CircularProgress, Radio, Typography} from "@mui/material";
 import styled from "styled-components";
-import 'react-device-emulator/lib/styles/style.css';
-import DeviceEmulator from "react-device-emulator";
 
 
 interface ThemeSelectorComponentInterface {
