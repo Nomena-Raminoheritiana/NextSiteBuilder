8fc93b9286c61f1e5850988d9d305d4cc1bf0394
Nomena Raminoheritiana <n.raminoheritiana@gmail.com>
2024-08-30 12:10:22 +0300

    [dev][builder][MainFloatingMenu] add bgColor/backdropFilter on the container

--- src/builder/src/components/Forms/Floating-menu/MainFloatingMenu.component.tsx
+++ src/builder/src/components/Forms/Floating-menu/MainFloatingMenu.component.tsx
@@ -9,9 +9,32 @@ interface MainFloatingMenuInterface {
     children?: ReactElement | HTMLElement | string
 }
 
+const mainFloatingStyle = {
+    position: 'fixed',
+    bottom: 16,
+    right: 16,
+    zIndex: 1200 ,
+    display:'flex',
+    gap:2,
+    px:2,
+    py:1,
+    '&::before': {
+        width:'100%',
+        height:'100%',
+        background: 'rgb(237,237,237,.5)',
+        backdropFilter:'blur(5px)',
+        position:'absolute',
+        top:0,
+        left:0,
+        content:'""',
+        borderRadius:'10px'
+    }
+
+}
+
 const MainFloatingMenuComponent:React.FC<MainFloatingMenuInterface> = ({children}) => {
     const isMobile = useIsMobile()
-    return  <Box sx={{ position: 'fixed', bottom: 16, right: 16, zIndex: 1200 , display:'flex', gap:2}}>
+    return  <Box sx={mainFloatingStyle}>
         <ThemeSelectorMenuComponent />
         {children}
         {!isMobile && <MobileViewMenuComponent />}
